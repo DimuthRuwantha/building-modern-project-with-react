16625d13539b80ee0aae57f97510d02042e4e0ea
DimuthR <dimuthruwantha@gmail.com>
2020-04-11 19:31:59 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider } from 'react-redux';
+
+import { configureStore } from './store';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import './index.css';
+
 ReactDOM.render(
   <React.StrictMode>
+    <Provider store={configureStore()} >
     <App />
+    </Provider>
+    
   </React.StrictMode>,
   document.getElementById('root')
 );
