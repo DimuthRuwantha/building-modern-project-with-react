798ef84a52005fc7135747a10b172bcd83713204
DimuthR <dimuthruwantha@gmail.com>
2020-04-12 22:38:54 +0530

    create delete requests

--- src/todos/TodoListItem.js
+++ src/todos/TodoListItem.js
@@ -5,13 +5,13 @@ const TodoListItem = ({ todo, onRemovePressed, onCompletedPressed }) => (
     <div className="todo-item-container">
         <h3>{todo.text}</h3>
         <div className="buttons-container">
-        {todo.isCompleted
+            {todo.isCompleted
                 ? null
                 : <button
                     onClick={() => onCompletedPressed(todo.text)}
                     className="completed-button">Mark As Completed</button>}
             <button
-                onClick={() => onRemovePressed(todo.text)}
+                onClick={() => onRemovePressed(todo.id)}
                 className="remove-button">Remove</button>
         </div>
     </div>
